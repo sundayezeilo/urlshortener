785ff3773b9d5469b3c52f5ef1032540d2987319
sundayezeilo <ezeilo.su@gmail.com>
2026-01-03 14:29:43 +0000

    refactor: add errx package for structured errors

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: migrate-create, clean-sqlc, sqlc, fmt, lint
+.PHONY: migrate-create, clean-sqlc, sqlc, fmt, lint, test
 
 migrate-create:
 	migrate create -ext sql -dir db/migrations $(name)  # Create migration file with make migrate-create name=<name>
@@ -14,3 +14,10 @@ fmt:
 
 lint:
 	golangci-lint run
+
+test:
+	go test -v ./...
+
+coverage:
+	go test -coverprofile=coverage.out -v ./...
+	go tool cover -html=coverage.out
---
+++ internal/errx/errx.go
@@ -0,0 +1,63 @@
+package errx
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Kind uint8
+
+const (
+	Unknown Kind = iota
+	NotFound
+	Conflict
+	Invalid
+	Unauthorized
+	Forbidden
+	Unavailable
+)
+
+type Error struct {
+	Op   string
+	Kind Kind
+	Err  error
+}
+
+func E(op string, kind Kind, err error) error {
+	if err == nil {
+		return nil
+	}
+	return &Error{
+		Op:   op,
+		Kind: kind,
+		Err:  err,
+	}
+}
+
+func (e *Error) Error() string {
+	if e.Err == nil {
+		return e.Op
+	}
+	if e.Op == "" {
+		return e.Err.Error()
+	}
+	return fmt.Sprintf("%s: %v", e.Op, e.Err)
+}
+
+func (e *Error) Unwrap() error { return e.Err }
+
+func KindOf(err error) Kind {
+	var e *Error
+	if errors.As(err, &e) {
+		return e.Kind
+	}
+	return Unknown
+}
+
+func OpOf(err error) string {
+	var e *Error
+	if errors.As(err, &e) {
+		return e.Op
+	}
+	return ""
+}
