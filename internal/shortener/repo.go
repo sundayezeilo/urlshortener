77ace9f3511f378c75f65da118d8379be6da2f06
sundayezeilo <ezeilo.su@gmail.com>
2026-01-06 00:55:01 +0000

    Add documentation for Repository interface

--- internal/shortener/repo.go
+++ internal/shortener/repo.go
@@ -2,6 +2,10 @@ package shortener
 
 import "context"
 
+// Repository defines the persistence operations for Link entities.
+// It abstracts the underlying data store and is responsible for
+// creating, retrieving, updating, and deleting links, as well as
+// tracking access-related metadata.
 type Repository interface {
 	CreateLink(ctx context.Context, link Link) (Link, error)
 	GetLinkBySlug(ctx context.Context, slug string) (Link, error)
